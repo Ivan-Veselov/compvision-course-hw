4beea8bc04f6e5c8e5a7e09dec1784935335ca24
Ivan Veselov <veselov.iva@gmail.com>
2019-03-14 21:04:23 +0300

    Implement initialization step of tracking process.

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -1,4 +1,7 @@
 #! /usr/bin/env python3
+from cv2.cv2 import findFundamentalMat, findHomography, RANSAC
+
+from _corners import FrameCorners
 
 __all__ = [
     'track_and_calc_colors'
@@ -14,11 +17,116 @@ import frameseq
 from _camtrack import *
 
 
+W_MATRIX = np.array([
+    [0., -1., 0.],
+    [1., 0., 0.],
+    [0., 0., 1.]
+])
+
+
+class CameraTracker:
+    def __init__(self, corner_storage: CornerStorage, intrinsic_mat: np.ndarray):
+        max_id = np.amax(np.concatenate([corners.ids for corners in corner_storage]))
+
+        self.__id_to_position = [None for _ in range(max_id + 1)]
+        self.__frame_matrix = [None for _ in corner_storage]
+
+        self.__corner_storage = corner_storage
+        self.__intrinsic_mat = intrinsic_mat
+
+        self.__triangulation_parameters = TriangulationParameters(max_reprojection_error=1, min_triangulation_angle_deg=1., min_depth=0.)
+        # todo: configure
+
+        self.__track_initialization()
+
+    def __calculate_pose(self, frame_corners1: FrameCorners, frame_corners2: FrameCorners):
+        correspondences = build_correspondences(frame_corners1, frame_corners2)
+
+        F, mask = findFundamentalMat(correspondences.points_1, correspondences.points_2)  # todo: have parameters
+        mask = mask.reshape(-1)
+        filtered_correspondences = build_correspondences(frame_corners1, frame_corners2, np.nonzero(1 - mask)[0])
+
+        fundamental_inliers = np.count_nonzero(mask)
+
+        E = self.__intrinsic_mat.T.dot(F).dot(self.__intrinsic_mat)
+
+        U, s, Vt = np.linalg.svd(E)
+
+        R1 = U.dot(W_MATRIX).dot(Vt)
+        R2 = U.dot(W_MATRIX.T).dot(Vt)
+        if np.linalg.det(R1) < 0:
+            R1 *= -1
+            R2 *= -1
+
+        t1 = U[:, 2]
+        t2 = -t1
+
+        possible_poses = [Pose(R1, t1), Pose(R1, t2), Pose(R2, t1), Pose(R2, t2)]
+
+        pose_cloud_size = []
+        for pose in possible_poses:
+            positions, ids = triangulate_correspondences(
+                filtered_correspondences,
+                eye3x4(),
+                pose_to_view_mat3x4(pose),
+                self.__intrinsic_mat,
+                self.__triangulation_parameters
+            )
+
+            pose_cloud_size.append(ids.shape[0])
+
+        index = np.argmax(pose_cloud_size)
+        if pose_cloud_size[index] == 0:
+            return None, None
+
+        pose = possible_poses[index]
+
+        H, mask = findHomography(correspondences.points_1, correspondences.points_2, RANSAC)  # todo: configure
+        mask = mask.reshape(-1)
+
+        homography_inliers = np.count_nonzero(mask)
+
+        return pose, fundamental_inliers / homography_inliers
+
+    def __track_initialization(self):
+        poses, qualities = zip(*[self.__calculate_pose(self.__corner_storage[0], i) for i in self.__corner_storage])
+        index = np.nanargmax(np.array(qualities, dtype=np.float32))
+
+        self.__frame_matrix = [eye3x4() for i in self.__corner_storage]
+        self.__frame_matrix[index] = pose_to_view_mat3x4(poses[index])
+        self.__update_cloud(0, index)
+
+    def __update_cloud(self, frame_id1, frame_id2):
+        correspondences = build_correspondences(self.__corner_storage[frame_id1], self.__corner_storage[frame_id2])
+
+        positions, ids = triangulate_correspondences(
+            correspondences,
+            self.__frame_matrix[frame_id1],
+            self.__frame_matrix[frame_id2],
+            self.__intrinsic_mat,
+            self.__triangulation_parameters
+        )
+
+        for pos, id in zip(positions, ids):
+            self.__id_to_position[id] = pos
+
+    def get_frame_matrices(self) -> List[np.ndarray]:
+        return self.__frame_matrix
+
+    def get_cloud_builder(self):
+        ids = np.array(list(map(lambda x: x[0], filter(lambda x: x[1] is not None, enumerate(self.__id_to_position)))))
+        positions = np.array([self.__id_to_position[i] for i in ids])
+
+        return PointCloudBuilder(ids, positions)
+
+
 def _track_camera(corner_storage: CornerStorage,
                   intrinsic_mat: np.ndarray) \
         -> Tuple[List[np.ndarray], PointCloudBuilder]:
-    # TODO: implement
-    return [], PointCloudBuilder()
+
+    tracker = CameraTracker(corner_storage, intrinsic_mat)
+
+    return tracker.get_frame_matrices(), tracker.get_cloud_builder()
 
 
 def track_and_calc_colors(camera_parameters: CameraParameters,
